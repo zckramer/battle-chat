df22db96f2946575b908544d482239a747b13307
Zachary Kramer <zckramer83@gmail.com>
2021-04-06 16:15:11 -0400

    basic chat in place

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+// });
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
-});
+})
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
