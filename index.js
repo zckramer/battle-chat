c463e541e7719b30e93950bd5cad38ef3edb4465
Zachary Kramer <zckramer83@gmail.com>
2021-04-08 16:14:40 -0400

    hooked up react client to backend

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+    console.log('a user connected: ', socket.id);
+
+    socket.on('join_room', (data) => {
+      socket.join(data);
+      console.log('user ', socket.id, ' joined room: ', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 // io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected');
+//     socket.on('chat message', (msg) => {
+//         io.emit('chat message', msg);
 //     });
+// })
+
+// http.listen(3001, () => {
+//   console.log('listening on *:3001');
 // });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-})
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+app.listen(3001, () => {
+  console.log('listening on *:3001');
 });
