55bc315820eddf38d36e9a9d4b94a93b7ed3c8b9
Zachary Kramer <zckramer83@gmail.com>
2024-01-28 16:43:45 -0500

    basic chat message for front end, basic return of getMessages for front end

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
-const socket = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+	cors: {
+		origin: 'http://localhost:4200',
+	},
+});
 const cors = require('cors');
 
 app.use(cors());
@@ -14,21 +16,26 @@ app.use(express.json());
 
 server.listen('3002', () => {
 	console.log('Server Running on Port 3002...');
+	console.log(__dirname);
 });
 
 app.get('/', (req, res) => {
-	res.sendFile(join(__dirname, 'index.html'));
-  console.log(__dirname)
+	res.sendFile(
+		'C:/Users/zckra/Documents/Projects/ng-battle-chat/src/index.html',
+	);
 });
 
 io.on('connection', socket => {
-	console.log(socket.id);
-
 	console.log('User Joined Room: ' + socket.id);
 
-	socket.on('chat message', msg => {
-		console.log('message: ', msg);
-		io.emit('chat message', msg);
+	socket.on('new-message', (message) => {
+		console.log('inside new-message...');
+		console.log(message);
+		io.emit('new-message', message);
+	});
+
+	socket.on('welcome', arg => {
+		console.log('hellooooooooo' + arg);
 	});
 
 	socket.on('disconnect', () => {
