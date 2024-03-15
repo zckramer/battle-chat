0a4a35818d016dadbe81b96e525e366a9ab08c99
Shannon Adams <77690999+shanfrom90@users.noreply.github.com>
2024-03-15 16:15:35 -0400

    add gitignore

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const cors = require('cors');
+const BACKEND_ENDPOINT = 'localhost:8080/api/messages'
 
 app.use(cors());
 app.use(express.json());
 
-server.listen('3002', () => {
-	console.log('Server Running on Port 3002...');
+server.listen('8081', () => {
+	console.log('Server Running on Port 8081...');
 });
 
 app.get('/', (req, res) => {
@@ -25,6 +26,10 @@ io.on('connection', socket => {
 	console.log(socket.id);
 
 	console.log('User Joined Room: ' + socket.id);
+
+	axios.get(BACKEND_ENDPOINT).then(response => {
+		console.log(response);
+	});
 
 	socket.on('chat message', msg => {
 		console.log('message: ', msg);
